97f9d35af72b370587ca344d6e331aa64cf666c0
Ilhamsyahids <ilhamsyahids@gmail.com>
2020-02-12 19:37:57 +0700

    implements readsector and writesector

--- kernel.c
+++ kernel.c
@@ -2,8 +2,8 @@
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 // void readString(char *string) {}
-// void readSector(char *buffer, int sector) {}
-// void writeSector(char *buffer, int sector) {}
+void readSector(char *buffer, int sector);
+void writeSector(char *buffer, int sector);
 // void readFile(char *buffer, char *filename, int *success) {}
 // void clear(char *buffer, int length) {} //Fungsi untuk mengisi buffer dengan 0
 // void writeFile(char *buffer, char *filename, int *sectors) {}
@@ -21,6 +21,32 @@ void printString(char *string) {
     interrupt(0x10, (0xE * 256) + string[i], 0, 0);
     i++;
   }
+}
+
+int div(int a, int b) {
+  int count = 0;
+  while (b < a) {
+    a -= b;
+    count++;
+  }
+  return count;
+}
+
+int mod(int a, int b) {
+  while (b < a) {
+    a -= b;
+  }
+  return a;
+}
+
+void readSector(char *buffer, int sector) {
+  interrupt(0x13, 0x201, buffer, div(sector, 36) * 0x100 + mod(sector, 18) + 1,
+            mod(div(sector, 18), 2) * 0x100);
+}
+
+void readSector(char *buffer, int sector) {
+  interrupt(0x13, 0x301, buffer, div(sector, 36) * 0x100 + mod(sector, 18) + 1,
+            mod(div(sector, 18), 2) * 0x100);
 }
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX) {
