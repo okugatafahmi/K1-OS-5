5a236b762c721182289d21f5d2b15e9cfd2139d5
okugatafahmi <otasrit@gmail.com>
2020-02-12 21:06:26 +0700

    change function position

--- kernel.c
+++ kernel.c
@@ -8,9 +8,11 @@
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
+int div(int,int);
+int mod(int,int);
 void readSector(char *buffer, int sector);
 void writeSector(char *buffer, int sector);
-void readFile(char *buffer, char *filename, int *success) {}
+void readFile(char *buffer, char *filename, int *success);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
 // void writeFile(char *buffer, char *filename, int *sectors) {}
 void executeProgram(char *filename, int segment, int *success);
@@ -24,22 +26,32 @@ int main() {
   while (1);
 }
 
-void executeProgram(char *filename, int segment, int *success) {
-  int i;
-  char buffer[512 * 20];
-  readFile(&buffer, filename, success);
-  if (*success) {
-    for (i = 0; i < 20*512; i++) {
-      putInMemory(segment, i, buffer[i])​;
-    }
-    launchProgram(segment);
-  }
-}
-
-void clear(char *buffer, int length) {
-  int i;
-  for (i = 0; i < length; i++) {
-    buffer[i] = 0x00;
+void handleInterrupt21 (int AX, int BX, int CX, int DX) {
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    case 0x2:
+      readSector(BX, CX);
+      break;
+    case 0x3:
+      writeSector(BX, CX);
+      break;
+    case 0x4:
+      readFile(BX, CX, DX);
+      break;
+    case 0x5:
+      // writeFile(BX, CX, DX);
+      break;
+    case 0x6:
+      executeProgram(BX, CX, DX);
+      break;
+    default:
+      printString("Invalid interrupt");
+      break;
   }
 }
 
@@ -75,7 +87,6 @@ void readString(char *string){
   string[i] = 0x0;
 }
 
-
 int div(int a, int b) {
   int count = 0;
   while (b < a) {
@@ -100,35 +111,6 @@ void readSector(char *buffer, int sector) {
 void writeSector(char *buffer, int sector) {
   interrupt(0x13, 0x301, buffer, div(sector, 36) * 0x100 + mod(sector, 18) + 1,
             mod(div(sector, 18), 2) * 0x100);
-}
-
-void handleInterrupt21 (int AX, int BX, int CX, int DX) {
-  switch (AX) {
-    case 0x0:
-      printString(BX);
-      break;
-    case 0x1:
-      // readString(BX);
-      break;
-    case 0x2:
-      // readSector(BX, CX);
-      break;
-    case 0x3:
-      // writeSector(BX, CX);
-      break;
-    case 0x4:
-      // readFile(BX, CX, DX);
-      break;
-    case 0x5:
-      // writeFile(BX, CX, DX);
-      break;
-    case 0x6:
-      // executeProgram(BX, CX, DX);
-      break;
-    default:
-      // printString("Invalid interrupt");
-      break;
-  }
 }
 
 void readFile(char *buffer, char *filename, int *success){
@@ -182,4 +164,23 @@ void readFile(char *buffer, char *filename, int *success){
     
   }
   *success = found;
+}
+
+void clear(char *buffer, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    buffer[i] = 0x00;
+  }
+}
+
+void executeProgram(char *filename, int segment, int *success) {
+  int i;
+  char buffer[512 * 20];
+  readFile(&buffer, filename, success);
+  if (*success) {
+    for (i = 0; i < 20*512; i++) {
+      putInMemory(segment, i, buffer[i])​;
+    }
+    launchProgram(segment);
+  }
 }
