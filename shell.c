1157ed1de6ef7efdb69305e30ee95f6cf6bf2807
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2020-03-12 14:17:33 +0700

    cd namaFolder, cd.., run file (belum di debug)

---
+++ shell.c
@@ -0,0 +1,179 @@
+#define SECTOR_SIZE 512
+#define MAP_SECTOR 0x100
+#define FILES_SECTOR 0X101
+#define SECTORS_SECTOR 0x103
+#define MAX_FILES 64
+#define MAX_SECTORS_FILESECTOR 32
+#define FILES_ENTRY_LENGTH 16
+#define MAX_FILENAME 14
+#define MAX_FILESECTOR 16
+#define EMPTY 0x00
+#define USED 0xFF
+#define FILE_NOT_FOUND -1
+#define FILE_HAS_EXIST -1
+#define INSUFFICIENT_FILES -2
+#define INSUFFICIENT_SECTORS -3
+#define INVALID_FOLDER -4
+#define TRUE 1
+#define FALSE 0
+#define CD 0
+#define RUN_FILE 1
+#define EXIT_PROGRAM 2
+
+void readString(char *string);
+void printString(char *string);
+int commandType(char *command
+char compare2String(char* s1, char* s2);
+
+int main(){
+	char isRun = true;
+	char success;
+	char command[SECTOR_SIZE];
+	int type, i, idxPathNow;
+	int idxNext, parentIndex;
+	int idxNow = 0xFF;
+	char isFound;
+	char namaNow[SECTOR_SIZE], temp[SECTOR_SIZE], pathNow[SECTOR_SIZE];
+
+	// nama root
+	namaNow[0] = '/';
+	namaNow[1] = '\0';
+
+	// path awal = root (/)
+	pathNow[0] = '/';
+	pathNow[1] = '\0';
+	idxPathNow = 1;
+
+	while(isRun){
+		printString(pathNow);
+		readString(command);
+
+		type = commandType(command);
+		if (type==CD){
+			if (command[2]=='.'){// cd.. (asumsikan gacuma cd. doang dan input pasti benar)
+				// ke index parent
+				idxNow = files[idxNow*FILES_ENTRY_LENGTH];
+
+				// kurangi pathnya (potong belakangnya)
+				pathNow[--idxPathNow] = '\0';
+				for (i = idxPathNow-1; pathNow[i]!='/'; --i){
+					pathNow[i] = '\0';
+				}
+
+				// ganti nama folder
+				for (i = 0; i<SECTOR_SIZE; ++i){
+					namafile[i] = files[idxNow*FILES_ENTRY_LENGTH+2+i];
+				}
+			}
+			else{// cd namafolder
+				// copy namafolder
+				i = 3;
+				while(command[i]!='\0'){
+					temp[i-3] = command[i];
+					i++;
+				}
+				temp[i-3] = '\0';
+
+				// go to folder yang di cd in
+				idxNext = 0;
+				isFound = FALSE;
+				parentIndex = idxNow;
+				while(idxNext<MAX_FILES && !isFound){
+			      if (files[idxNext*FILES_ENTRY_LENGTH]==parentIndex && compare2String(files+idxNext*FILES_ENTRY_LENGTH+2,temp)){
+			        isFound = TRUE;
+			      }
+			      else ++idxNext;
+				}
+
+				// ganti jadi index baru
+				idxNow = idxNext;
+
+				// copy namafile baru
+				for (i = 0; temp[i]!='\0'; ++i){
+					namaNow[i] = temp[i];
+					pathNow[idxPathNow++] = temp[i];
+				}
+				namaNow[i] = '\0';
+				pathNow[idxPathNow++] = '/';
+				pathNow[idxPathNow] = '\0';
+
+			}
+		}
+		else if (type==RUN_FILE){ // ./namafile
+			i = 2;
+			while(command[i]!='\0'){	// copy namafile yang akan di run
+				temp[i-2] = command[i];
+				i++;
+			}
+			temp[i-2] = '\0';
+			interrupt(0x21, idxNow, temp, 0x2000, &success);
+			if (success != 1)
+		    {
+		    	interrupt(0x21, 0x0, "Failed to execute file\n\r", 0, 0);
+		    }
+
+		}
+		else if (type==EXIT_PROGRAM){
+			isRun = false;
+		}
+		else{
+			printString("input salah");
+		}
+	}
+}
+
+char compare2String(char* s1, char* s2){
+  int i;
+  for (i = 0; i<MAX_FILENAME; ++i){
+    if (s1[i]=='\0' && s2[i]=='\0') return TRUE;
+    if (s1[i]!=s2[i]) return FALSE;
+  }
+}
+
+
+int commandType(char *command){
+	if (command[0]=='c' && command[1]=='d') return CD;
+	else if (command[0]=='.' && command[1]=='/') return RUN_FILE;
+	else if (compare2String("exit",command)) return EXIT_PROGRAM;
+}
+
+void printString(char *string)
+{
+  int i = 0;
+  while (string[i] != '\0')
+  {
+    interrupt(0x10, (0xE * 256) + string[i], 0, 0);
+    i++;
+  }
+}
+
+void readString(char *string)
+{
+  int i = 0;
+  char input = 0;
+  while (input != '\r')
+  {
+    input = interrupt(0x16, 0, 0, 0, 0);
+
+    if (input == '\b')
+    {
+      interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+      interrupt(0x10, 0xE00 + '\0', 0, 0, 0);
+      interrupt(0x10, 0xE00 + '\b', 0, 0, 0);
+      if (i > 0)
+      {
+        i--;
+      }
+    }
+    else
+    {
+      string[i] = input;
+      interrupt(0x10, 0xE00 + input, 0, 0, 0);
+      if (input != '\r')
+        i++;
+    }
+  }
+  string[i] = '\0';
+  interrupt(0x10, 0xE00 + '\n', 0, 0, 0);
+  interrupt(0x10, 0xE00 + '\r', 0, 0, 0);
+}
