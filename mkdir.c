59071c3d91405a5264f6d47efd1b8da7d0a961c1
okugatafahmi <otasrit@gmail.com>
2020-04-03 06:52:25 +0700

    edit mkdir

--- mkdir.c
+++ mkdir.c
@@ -1,17 +1,36 @@
-#include "defines.h"
-#include "folder.h"
+#include "lib/defines.h"
+#include "lib/folder.h"
 
 int main() {
-    char args[SECTOR_SIZE];
-    char currentFolder;
-    char argc;
-    char argv[1][ARGS_LENGTH + 1];
-    int succI;
-  
-    interrupt(0x21, 0x02, args, SECTOR_SIZE, 0);
-    currentFolder = args[0];
+    char parentIndex, argc, argv[MAX_ARGS*ARGS_LENGTH];
+    int i, success;
 
-    //Keluar
-    interrupt(0x21, 0x20, currentFolder, 0, 0);
-    interrupt(0x21, 0x7, ' ', 0, 0);
+    getArgs(&parentIndex, &argc, argv);
+    for (i=0; i<(int)argc; ++i){
+        createFolder(argv+i*ARGS_LENGTH,&success,parentIndex);
+        if (success==1){
+            printString("mkdir: succeed to create directory '");
+            printString(argv+i*ARGS_LENGTH);
+            printString("'");
+        }
+        else{
+            printString("mkdir: cannot create directory '");
+            printString(argv+i*ARGS_LENGTH);
+            if (success==FILE_HAS_EXIST){    
+                printString("' : File exists");
+            }
+            else if (success==INSUFFICIENT_FILES){
+                printString("' : Insufficient directory");
+            }
+            else if (success==-3){
+                printString("' : No such file or directory");
+            }
+            else if (success==-4){
+                printString("' : Not a directory");
+            }
+            
+        }
+        printString("\n\r");
+    }
+    while(1){}
 }
